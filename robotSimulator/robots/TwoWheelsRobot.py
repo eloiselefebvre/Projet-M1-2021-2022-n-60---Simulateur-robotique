6ee72a42c82429d4f13fdad97c8b34eff80f1cef
lpl-dev <leo.planquette@isen-ouest.yncrea.fr>
2022-02-23 16:27:38 +0100

    Modification de l'utilisation des marques d'orientation des rectangles

--- robotSimulator/robots/TwoWheelsRobot.py
+++ robotSimulator/robots/TwoWheelsRobot.py
@@ -19,6 +19,7 @@ class TwoWheelsRobot(Robot):
     def __init__(self,color=None,robotWidth=50,robotHeight=60,distanceBetweenWheels=50,wheelsRadius=10,wheelYPos=0):
         self._color = random.choice(self.COLORS) if color is None else color
         rep=Rectangle(robotWidth,robotHeight,self._color,self.DEFAULT_BORDER_RADIUS)
+        rep.addOrientationMark()
         super().__init__(Representation(rep))
         self._leftWheel = Wheel(wheelsRadius, self.DEFAULT_WHEEL_WIDTH)
         self._rightWheel = Wheel(wheelsRadius, self.DEFAULT_WHEEL_WIDTH)
