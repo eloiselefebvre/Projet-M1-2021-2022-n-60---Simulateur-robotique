6bd85f5ee0ed5c4c08c6885c2d33400c29cbde79
EloiseLEFEBVRE <eloise.lefebvre@isen-ouest.yncrea.fr>
2022-03-09 09:58:18 +0100

    Possibilité de générer un tracé depuis l'interface

--- robotSimulator/ressources/maps/Path.py
+++ robotSimulator/ressources/maps/Path.py
@@ -14,21 +14,19 @@ class Path:
     def  __init__(self, environment):
         self._environment = environment
         self._pathElements = []
-        self.drawPath()
 
     def drawPath(self):
 
-        # numberOfCircle=random.randint(1,2)
-        numberOfCircle=2
-        print(numberOfCircle)
+        numberOfCircle=random.randint(1,2)
 
         if numberOfCircle==1:
             radius=random.randint(200,int(self._environment.getWidth()/2))
-            print(radius)
             circle1 = Object(Representation(Circle(radius,"#444444")))
             circle2 = Object(Representation(Circle(radius-20,"#f0f0f0")))
             self._environment.addVirtualObject(circle1,int(self._environment.getWidth()/2),int(self._environment.getHeight()/2))
             self._environment.addVirtualObject(circle2, int(self._environment.getWidth() / 2),int(self._environment.getHeight() / 2))
+            self._pathElements.append(circle1)
+            self._pathElements.append(circle2)
 
         else:
             radius1=random.randint(200,int(self._environment.getWidth()/4))
@@ -41,5 +39,18 @@ class Path:
             self._environment.addVirtualObject(circle2, int(self._environment.getWidth()/2)-radius1,int(self._environment.getHeight() / 2))
             self._environment.addVirtualObject(circle3, int(self._environment.getWidth()/2)-20+radius2,int(self._environment.getHeight() / 2))
             self._environment.addVirtualObject(circle4, int(self._environment.getWidth()/2)-20+radius2,int(self._environment.getHeight() / 2))
+            self._pathElements.append(circle1)
+            self._pathElements.append(circle2)
+            self._pathElements.append(circle2)
+            self._pathElements.append(circle3)
+
+    def deleteElements(self):
+        for item in self._pathElements:
+            self._environment.removeVirtualObject(item)
+        self._pathElements = []
+
+    def getElements(self):
+        return self._pathElements
+
 
 
--- robotSimulator/tests/followLineWithColorSensor.py
+++ robotSimulator/tests/followLineWithColorSensor.py
@@ -1,8 +1,5 @@
 import time
 
-from robotSimulator import Object
-from robotSimulator.representation import Representation
-from robotSimulator.representation.shapes import Rectangle, Circle
 from robotSimulator.ressources.maps.Path import Path
 from robotSimulator.robots import TwoWheelsRobot
 from robotSimulator.sensors.ColorSensor import ColorSensor
@@ -22,40 +19,8 @@ def followLineWithColorSensor():
     rob1.setRightWheelSpeed(200)
     rob1.setLeftWheelSpeed(200)
 
-    # obs1=Object(Representation(Rectangle(980,20,"#444")))
-    # obs2=Object(Representation(Rectangle(980,20,"#444")))
-    # obs3=Object(Representation(Rectangle(980,20,"#444")))
-    # obs4=Object(Representation(Rectangle(980,20,"#444")))
-    # c1=Object(Representation(Circle(20,"#444")))
-    # c2=Object(Representation(Circle(20,"#444")))
-    # c3=Object(Representation(Circle(20,"#444")))
-    # c4=Object(Representation(Circle(20,"#444")))
-    # c5=Object(Representation(Circle(20,"#f0f0f0")))
-    # c6=Object(Representation(Circle(20,"#f0f0f0")))
-    # c7=Object(Representation(Circle(20,"#f0f0f0")))
-    # c8=Object(Representation(Circle(20,"#f0f0f0")))
-    #
-    #
-    # envWidth=1500
-    # envHeight=1500
-    # env=Environment(envWidth,envHeight)
-    # env.addObject(rob1,250,250)
-    # env.addVirtualObject(obs1,envWidth-250,envHeight/2,-90)
-    # env.addVirtualObject(obs2,envWidth/2,envHeight-250)
-    # env.addVirtualObject(obs3,250,envHeight/2,-90)
-    # env.addVirtualObject(obs4,envWidth/2,250)
-    # env.addVirtualObject(c1,250+10,250+10)
-    # env.addVirtualObject(c2,250+10,envHeight-250-10)
-    # env.addVirtualObject(c3,envWidth-250-10,250+10)
-    # env.addVirtualObject(c4,envHeight-250-10,envWidth-250-10)
-    # env.addVirtualObject(c5, 250 + 30, 250 + 30)
-    # env.addVirtualObject(c6, 250 + 30, envHeight - 250 - 30)
-    # env.addVirtualObject(c7, envWidth - 250 - 30, 250 + 30)
-    # env.addVirtualObject(c8, envHeight - 250 - 30, envWidth - 250 - 30)
-
     env=Environment(1500,1500)
     env.addObject(rob1,250,250)
-    Path(env)
 
     sim = Simulation(env)
     sim.run()
@@ -63,22 +28,19 @@ def followLineWithColorSensor():
 
     while True:
 
-        # print("right",colorSensorRight.getValue())
-        # print("left",colorSensorLeft.getValue())
-        #
-        # if colorSensorRight.getValue()=="#444444" and colorSensorLeft.getValue()=="#444444":
-        #     rob1.setLeftWheelSpeed(FORWARD_SPEED)
-        #     rob1.setRightWheelSpeed(FORWARD_SPEED)
-        # elif colorSensorRight.getValue()!="#444444" and colorSensorLeft.getValue()=="#444444":
-        #     rob1.setLeftWheelSpeed(TURN_SPEED)
-        #     rob1.setRightWheelSpeed(-TURN_SPEED)
-        # elif colorSensorRight.getValue()=="#444444" and colorSensorLeft.getValue()!="#444444":
-        #     rob1.setRightWheelSpeed(TURN_SPEED)
-        #     rob1.setLeftWheelSpeed(-TURN_SPEED)
-        # else:
-        #     print("Out of path !")
-        #     rob1.setLeftWheelSpeed(0)
-        #     rob1.setRightWheelSpeed(0)
+        if colorSensorRight.getValue()=="#444444" and colorSensorLeft.getValue()=="#444444":
+            rob1.setLeftWheelSpeed(FORWARD_SPEED)
+            rob1.setRightWheelSpeed(FORWARD_SPEED)
+        elif colorSensorRight.getValue()!="#444444" and colorSensorLeft.getValue()=="#444444":
+            rob1.setLeftWheelSpeed(TURN_SPEED)
+            rob1.setRightWheelSpeed(-TURN_SPEED)
+        elif colorSensorRight.getValue()=="#444444" and colorSensorLeft.getValue()!="#444444":
+            rob1.setRightWheelSpeed(TURN_SPEED)
+            rob1.setLeftWheelSpeed(-TURN_SPEED)
+        else:
+            # print("Out of path !")
+            rob1.setLeftWheelSpeed(0)
+            rob1.setRightWheelSpeed(0)
 
         time.sleep(.01)
 
