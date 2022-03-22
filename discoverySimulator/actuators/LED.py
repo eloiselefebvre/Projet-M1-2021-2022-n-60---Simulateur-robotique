3468aeb45f1478bc5b0d9164c7fa6b453cea21c5
EloiseLEFEBVRE <eloise.lefebvre@isen-ouest.yncrea.fr>
2022-03-22 13:48:37 +0100

    Documentation Actuator

--- discoverySimulator/actuators/LED.py
+++ discoverySimulator/actuators/LED.py
@@ -4,21 +4,35 @@ from discoverySimulator.representation.shapes import Circle
 
 
 class LED(Actuator):
+
     RED = "#f00"
     GREEN = "#0f0"
     BLUE = "#00f"
     YELLOW = "#ff0"
-
     LOW=0
     HIGH=1
 
-    def __init__(self,color):
+    def __init__(self,color:str):
+        """
+        This method is used to create a new LED
+        :param color: color of the LED
+        """
         self._representation = Representation(Circle(5,color,20))
         super().__init__(self._representation)
         self._state=0
         self._color = color
 
-    def setState(self,state):
+    def getState(self):
+        """
+        This method is used to get the state of a LED
+        :return: the state of the LED
+        """
+        return self._state
+
+    def getSpecifications(self):
+        return f"Current state : {'ON' if self._state else 'OFF'}"
+
+    def setState(self,state:bool):
         """
         This method allows to change the state of a LED
         :param state: state of the LED
@@ -31,12 +45,5 @@ class LED(Actuator):
                 self._representation.getShape().setOpacity(40)
         self.notifyObservers("stateChanged")
 
-    def getState(self):
-        """
-        This method is used to get the state of a LED
-        :return: the state of the LED
-        """
-        return self._state
 
-    def getSpecifications(self):
-        return f"Current state : {'ON' if self._state else 'OFF'}"
+
