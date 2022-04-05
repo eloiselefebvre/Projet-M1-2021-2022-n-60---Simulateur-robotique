e121adbabc115e404a0936776bd3cd9fba6982dd
lpl-dev <leo.planquette@isen-ouest.yncrea.fr>
2022-04-05 18:54:16 +0200

    Amélioration de la démo

--- discoverySimulator/demonstrations/scenario.py
+++ discoverySimulator/demonstrations/scenario.py
@@ -5,14 +5,14 @@ from discoverySimulator.obstacles import CircularObstacle, RectangularObstacle
 from discoverySimulator.representation import Representation
 from discoverySimulator.representation.shapes import Rectangle, Circle, Polygon
 from discoverySimulator.ressources.ReinforcementLearning import ReinforcementLearning
-from discoverySimulator.sensors import Telemeter, LIDAR
+from discoverySimulator.sensors import Telemeter, LIDAR, ColorSensor
 from discoverySimulator.simulation import Environment, Simulation
 from discoverySimulator.robots import RectangularTwoWheelsRobot, CircularTwoWheelsRobot
 from discoverySimulator.actuators import LED
 
 def scenario():
     scenario1()
-    # scenario2()
+    scenario2()
     scenario3()
     scenario4()
     scenario5()
@@ -107,6 +107,53 @@ def scenario1():
         mySimulation.sync()
     mySimulation.stop()
     mySimulation.closeInterface()
+
+def scenario2():
+    rob1 = RectangularTwoWheelsRobot()
+    rob1.enableOdometry()
+    FORWARD_SPEED = 300
+    TURN_SPEED = 100
+
+    colorSensorRight = ColorSensor()
+    colorSensorLeft = ColorSensor()
+
+    rob1.addComponent(colorSensorRight, 5, 25)
+    rob1.addComponent(colorSensorLeft, -5, 25)
+
+    env = Environment(1200, 800)
+    polygon = Polygon(
+        [(200, 500), (300, 450), (350, 300), (450, 250), (550, 330), (800, 150), (900, 160), (950, 220), (900, 400),
+         (860, 430), (780, 420), (720, 450),
+         (640, 560), (550, 610), (400, 560), (250, 620), (200, 580)], "#444")
+
+    env.addVirtualObject(Object(Representation(polygon)))
+    polygonOffset = polygon.offset(-30)
+    polygonOffset.setColor("#f0f0f0")
+    env.addVirtualObject(Object(Representation(polygonOffset)))
+
+    env.addObject(rob1, 250, 250)
+
+    sim = Simulation(env)
+    sim.run()
+    sim.showInterface()
+
+    while sim.time()<40:
+        if colorSensorRight.getValue() == "#444444" and colorSensorLeft.getValue() == "#444444":
+            rob1.setLeftWheelSpeed(FORWARD_SPEED)
+            rob1.setRightWheelSpeed(FORWARD_SPEED)
+        elif colorSensorRight.getValue() != "#444444" and colorSensorLeft.getValue() == "#444444":
+            rob1.setLeftWheelSpeed(-TURN_SPEED)
+            rob1.setRightWheelSpeed(TURN_SPEED)
+        elif colorSensorRight.getValue() == "#444444" and colorSensorLeft.getValue() != "#444444":
+            rob1.setLeftWheelSpeed(TURN_SPEED)
+            rob1.setRightWheelSpeed(-TURN_SPEED)
+        else:
+            rob1.setLeftWheelSpeed(0)
+            rob1.setRightWheelSpeed(0)
+        sim.sync()
+
+    sim.stop()
+    sim.closeInterface()
 
 def scenario3():
     lidar = LIDAR()
--- discoverySimulator/tests/road.py
+++ discoverySimulator/tests/road.py
@@ -1,10 +1,10 @@
-import time
 from discoverySimulator import Object
 from discoverySimulator.representation import Representation
-from discoverySimulator.representation.shapes import Line, Circle, Rectangle
+from discoverySimulator.representation.shapes import Circle, Rectangle
 from discoverySimulator.robots import RectangularTwoWheelsRobot
 from discoverySimulator.sensors.ColorSensor import ColorSensor
 from discoverySimulator.simulation import Environment, Simulation
+from discoverySimulator.representation.shapes import Polygon
 
 
 def roadDemo():
@@ -73,4 +73,49 @@ def roadDemo():
             # print("Out of path !")
             robot.setLeftWheelSpeed(0)
             robot.setRightWheelSpeed(0)
+        sim.sync()
+
+def road():
+    rob1 = RectangularTwoWheelsRobot()
+    rob1.enableOdometry()
+    FORWARD_SPEED = 300
+    TURN_SPEED = 100
+
+    colorSensorRight = ColorSensor()
+    colorSensorLeft = ColorSensor()
+
+
+    rob1.addComponent(colorSensorRight, 5, 25)
+    rob1.addComponent(colorSensorLeft, -5, 25)
+
+    env = Environment(1200, 800)
+    polygon = Polygon(
+        [(200, 500), (300, 450), (350, 300), (450, 250), (550, 330), (800, 150), (900, 160), (950, 220), (900, 400),
+         (860, 430), (780, 420), (720, 450),
+         (640, 560), (550, 610), (400, 560), (250, 620), (200, 580)], "#444")
+
+    env.addVirtualObject(Object(Representation(polygon)))
+    polygonOffset = polygon.offset(-30)
+    polygonOffset.setColor("#f0f0f0")
+    env.addVirtualObject(Object(Representation(polygonOffset)))
+
+    env.addObject(rob1, 250, 250)
+
+    sim = Simulation(env)
+    sim.run()
+    sim.showInterface()
+
+    while True:
+        if colorSensorRight.getValue() == "#444444" and colorSensorLeft.getValue() == "#444444":
+            rob1.setLeftWheelSpeed(FORWARD_SPEED)
+            rob1.setRightWheelSpeed(FORWARD_SPEED)
+        elif colorSensorRight.getValue() != "#444444" and colorSensorLeft.getValue() == "#444444":
+            rob1.setLeftWheelSpeed(-TURN_SPEED)
+            rob1.setRightWheelSpeed(TURN_SPEED)
+        elif colorSensorRight.getValue() == "#444444" and colorSensorLeft.getValue() != "#444444":
+            rob1.setLeftWheelSpeed(TURN_SPEED)
+            rob1.setRightWheelSpeed(-TURN_SPEED)
+        else:
+            rob1.setLeftWheelSpeed(0)
+            rob1.setRightWheelSpeed(0)
         sim.sync()
